f6cbff17af5c2566420468d9e0d8fd6d38601065
YoelAharoni12 <yoel345@gmail.com>
2023-07-02 00:24:09 +0300

    generate a link for event

--- apps/client/src/components/events/events.tsx
+++ apps/client/src/components/events/events.tsx
@@ -1,9 +1,10 @@
 import './events.less';
 import {db, eventRef} from "../../helpers/init-firebase";
-import {useEffect, useState} from "react";
-import {onValue, set, ref} from "firebase/database";
+import {useEffect, useRef, useState} from "react";
+import {increment, onValue, push, ref, set} from "firebase/database";
 import {Button, Container, Form, Grid, Modal, Segment} from "semantic-ui-react";
 import * as Yup from 'yup';
+
 
 type Event = {
   creationDate: string;
@@ -24,16 +25,26 @@ export interface LoginProps {
 }
 
 export function Events() {
-  const title = "Events"
   const [events, setEvents] = useState([])
   const [open, setOpen] = useState(false);
-  const [formData, setFormValues] = useState({name: '', description: '', storage: '', email: ''});
+  const [secondModalOpen, setSecondModalOpen] = useState(false);
+  const [formData, setFormValues] = useState({name: '', description: '', storage: '', email: '', url: ''});
+  const [link, setLink] = useState('');
+
+  const inputRef = useRef<HTMLInputElement>(null);
+  const handleCopyClick = () => {
+    if (inputRef.current) {
+      inputRef.current.select();
+      document.execCommand("copy");
+    }
+  };
 
   useEffect(() => {
     onValue(eventRef, (snapshot) => {
       setEvents(snapshot.val())
     });
   }, [])
+
   const handleChange = (e: any) => {
     const {name, value} = e.target;
 
@@ -42,18 +53,30 @@ export function Events() {
       [name]: value,
     }));
   };
+
   const handleSubmit = async (e: any) => {
     try {
       await eventSchema.validate(formData, {abortEarly: false});
       const date = new Date(Date.now());
       const formattedDate = `${date.getDate().toString().padStart(2, '0')}/${(date.getMonth() + 1).toString().padStart(2, '0')}/${date.getFullYear().toString()}`;
 
-      set(ref(db, 'events/' + formData.name), {...formData, creationDate: formattedDate})
+      const newEventRef = push(ref(db, 'events/' + formData.name), {...formData, creationDate: formattedDate});
+      const newEventId = newEventRef.key
+      const url = `https://only-me-2023.web.app/register-event/${newEventId}`
+      setLink(url)
+
+      setFormValues({name: '', description: '', storage: '', email: '', url: ''});
+      setOpen(false);
     } catch (e: any) {
       console.error(e)
     }
-    setOpen(false);
   };
+
+  const handleAddEvent = () => {
+    setSecondModalOpen(true);
+  }
+
+
   return (
     <div style={{display: 'flex', justifyContent: 'center'}}>
       <Segment>
@@ -61,7 +84,7 @@ export function Events() {
           <h2 style={{textAlign: 'center'}}>Upcoming Events</h2>
           <Grid columns={3}>
             {Object.values(events).map((event: Event, index) => (
-              <Grid.Row key={event.name}>
+              <Grid.Row key={index}>
                 <Grid.Column width={4}>
                   <img className="ui tiny image" src="../../assets/69DFE2D3-0914-4DDB-94BC-E425304646E7.jpg"/>
                 </Grid.Column>
@@ -88,34 +111,73 @@ export function Events() {
         style={{position: 'fixed', bottom: '0', right: '0'}}
         onClick={() => setOpen(true)}
       />
-      <Modal open={open} onClose={() => setOpen(false)}>
+      <Modal open={open} onClose={() => setOpen(true)}>
         <Modal.Header>Create Event</Modal.Header>
         <Modal.Content>
           <Form onSubmit={handleSubmit}>
             <Form.Field>
-              <label htmlFor={"nameID"}>Name</label>
-              <input id={"nameID"} name="name" onChange={handleChange} type="text" placeholder="Enter your name"/>
+              <label htmlFor="nameID">Name</label>
+              <input id="nameID" name="name" onChange={handleChange} type="text" placeholder="Enter the event name"
+                     required/>
             </Form.Field>
             <Form.Field>
-              <label htmlFor={"emailId"}>Email</label>
-              <input id={"emailId"} name="email" onChange={handleChange} type="email" placeholder="Enter your email"/>
+              <label htmlFor={'emailId'}>Email</label>
+              <input
+                id={'emailId'}
+                name="email"
+                onChange={handleChange}
+                type="email"
+                placeholder="Enter your email"
+              />
             </Form.Field>
             <Form.Field>
-              <label htmlFor={"storageId"}>storge</label>
-              <input id={"storageId"} name="storage" onChange={handleChange} type="tel"
-                     placeholder="enter the storage that you need..."/>
+              <label htmlFor={'storageId'}>storge</label>
+              <input
+                id={'storageId'}
+                name="storage"
+                onChange={handleChange}
+                type="tel"
+                placeholder="enter the storage that you need..."
+              />
             </Form.Field>
             <Form.Field>
-              <label htmlFor={"descriptionId"}>description</label>
-              <input id={"descriptionId"} name="description" onChange={handleChange} type="text"
-                     placeholder="description..."/>
+              <label htmlFor={'descriptionId'}>description</label>
+              <input
+                id={'descriptionId'}
+                name="description"
+                onChange={handleChange}
+                type="text"
+                placeholder="description..."
+              />
             </Form.Field>
             <div style={{textAlign: "center"}}>
-              <Button type="submit" primary>
-                Add event
-              </Button>
+              <Modal.Actions>
+                <Button onClick={handleAddEvent} type="submit" primary>
+                  Add event
+                </Button>
+              </Modal.Actions>
             </div>
           </Form>
+        </Modal.Content>
+      </Modal>
+      <Modal open={secondModalOpen} onClose={() => setSecondModalOpen(false)}>
+        <Modal.Header>Add New Event</Modal.Header>
+        <Modal.Content>
+          <Modal.Content>
+            <Form>
+              <Form.Field>
+                <label>Link</label>
+                <input
+                  disabled
+                  type="text"
+                  value={link}
+                  readOnly
+                  ref={inputRef}
+                />
+              </Form.Field>
+              <Button onClick={handleCopyClick}>Copy Link</Button>
+            </Form>
+          </Modal.Content>
         </Modal.Content>
       </Modal>
     </div>
