13f34d5b2e9837e856ed32ec53ef4ecafecbd72d
ohad ben shimol <a@a.com>
2023-07-07 01:27:48 +0300

    .

--- apps/client/src/components/events/events.tsx
+++ apps/client/src/components/events/events.tsx
@@ -1,45 +1,25 @@
-import * as Yup from 'yup';
-import { eventsRef } from '../../helpers/firebase';
-import { FC, useEffect, useRef, useState } from 'react';
-import { equalTo, onValue, orderByChild, query } from 'firebase/database';
-import { Button, Container, Grid, Modal, Segment } from 'semantic-ui-react';
+import ParticipantsEvents from '../participantsEvents/ParticipantsEvents';
+import { FC, useEffect, useState } from 'react';
+import { Button, Container, Modal, Segment } from 'semantic-ui-react';
 import { useSelector } from 'react-redux';
-import { getUser, getUserID } from '../../store/reducers/userSlice';
-import { Link, useNavigate } from 'react-router-dom';
-import { NewEvent } from '../../shared/models/event';
+import { getUser } from '../../store/reducers/userSlice';
+import { useNavigate } from 'react-router-dom';
 import { useCookies } from 'react-cookie';
-import ParticipantsEvents from '../owner-events/owner-events';
 import { CreateEvent } from '../createEvent/CreateEvent';
-import './events.less';
 import { ShareEvent } from '../shareEvent/ShareEvent';
+import { OwnerEvents } from '../ownerEvents/OwnerEvents';
+import './events.less';
 
-interface LoginProps {}
+interface EventsProps {}
 
-export const Events: FC<LoginProps> = () => {
-  const userID = useSelector(getUserID);
+export const Events: FC<EventsProps> = () => {
   const user = useSelector(getUser);
   const navigate = useNavigate();
-  const [ownerEvents, setOwnerEvents] = useState<NewEvent[]>([]);
-  const [open, setOpen] = useState(false);
+
   const [cookies] = useCookies(['user']);
+  const [createEventIsOpen, setCreateEventIsOpen] = useState(false);
   const [shareEventOpen, setShareEventOpen] = useState(false);
   const [link, setLink] = useState('');
-
-  useEffect(() => {
-    const eventQuery = query(
-      eventsRef,
-      orderByChild(`owner`),
-      equalTo(userID!)
-    );
-
-    onValue(eventQuery, (snapshot) => {
-      const data = snapshot.val() as Record<string, NewEvent>;
-      const EventsByUserID: NewEvent[] =
-        data && Object.values(data).filter((event) => event.owner === userID);
-      setOwnerEvents(EventsByUserID);
-      console.log('events that i am participant', data);
-    });
-  }, [userID]);
 
   useEffect(() => {
     if (!(user.email && cookies.user.email)) {
@@ -51,46 +31,20 @@ export const Events: FC<LoginProps> = () => {
   }, [user, cookies]);
 
   const onSubmit = () => {
-    setOpen(false);
+    setCreateEventIsOpen(false);
     setShareEventOpen(true);
   };
 
   const onCancel = () => {
-    setOpen(false);
+    setCreateEventIsOpen(false);
   };
 
+  const onClickAddEvent = () => setCreateEventIsOpen(true);
   return (
     <div style={{ display: 'flex', justifyContent: 'center' }}>
       <Segment>
         <Container>
-          {ownerEvents && (
-            <>
-              <h2 style={{ textAlign: 'center' }}>Upcoming Events</h2>
-              <Grid columns={3}>
-                {Object.values(ownerEvents)?.map((event: NewEvent, index) => (
-                  <Grid.Row key={index}>
-                    <Grid.Column width={4}>
-                      <img
-                        className="ui tiny image"
-                        src="../../assets/69DFE2D3-0914-4DDB-94BC-E425304646E7.jpg"
-                      />
-                    </Grid.Column>
-                    <Grid.Column width={8}>
-                      <p>{event.name}</p>
-                      <p>{event.description}</p>
-                      <p>{event.url}</p>
-                      <p>{event.creationDate}</p>
-                    </Grid.Column>
-                    <Grid.Column width={4}>
-                      <Link to={`/uploadFile/${event.id}`}>Upload images</Link>
-                      <Button>Details</Button>
-                    </Grid.Column>
-                  </Grid.Row>
-                ))}
-              </Grid>
-            </>
-          )}
-
+          <OwnerEvents />
           <ParticipantsEvents />
         </Container>
       </Segment>
@@ -100,9 +54,12 @@ export const Events: FC<LoginProps> = () => {
         size="huge"
         circular
         style={{ position: 'fixed', bottom: '0', right: '0' }}
-        onClick={() => setOpen(true)}
+        onClick={onClickAddEvent}
       />
-      <Modal open={open} onClose={() => setOpen(true)}>
+      <Modal
+        open={createEventIsOpen}
+        onClose={() => setCreateEventIsOpen(true)}
+      >
         <CreateEvent
           onCancel={onCancel}
           setLink={setLink}
@@ -115,14 +72,5 @@ export const Events: FC<LoginProps> = () => {
     </div>
   );
 };
-
-export const eventSchema = Yup.object().shape({
-  name: Yup.string().required('Name is required'),
-  description: Yup.string().required('Description is required'),
-  storage: Yup.number()
-    .required('Storage is required')
-    .positive('Storage must be a positive number'),
-  email: Yup.string().email(),
-});
 
 export default Events;
---
+++ apps/client/src/components/ownerEvents/OwnerEvents.tsx
@@ -0,0 +1,111 @@
+import { eventsRef, usersRef } from '../../helpers/firebase';
+import { FC, useEffect, useState } from 'react';
+import { equalTo, get, onValue, orderByChild, query } from 'firebase/database';
+import { Button, Container, Grid } from 'semantic-ui-react';
+import { useSelector } from 'react-redux';
+import { UserState, getUserID } from '../../store/reducers/userSlice';
+import { Link } from 'react-router-dom';
+import { NewEvent } from '../../shared/models/event';
+import { useQuery } from 'react-query';
+import userIMAGE from '../../assets/user.png';
+import './OwnerEvents.less';
+
+interface OwnerEventsProps {}
+
+export const OwnerEvents: FC<OwnerEventsProps> = () => {
+  const userID = useSelector(getUserID);
+  const [ownerEvents, setOwnerEvents] = useState<Record<string, NewEvent>>();
+  const [users, setUsers] = useState<Record<string, UserState>>();
+  useQuery('das', () => get(query(usersRef)), {
+    onError: console.error,
+    onSuccess: (data) => setUsers(data.val()),
+  });
+
+  useEffect(() => {
+    const eventQuery = query(
+      eventsRef,
+      orderByChild(`owner`),
+      equalTo(userID!)
+    );
+
+    onValue(eventQuery, (snapshot) => {
+      const data = snapshot.val() as Record<string, NewEvent>;
+      const EventsByUserID =
+        data &&
+        Object.fromEntries(
+          Object.entries(data).filter(([k, event]) => event.owner === userID)
+        );
+      setOwnerEvents(EventsByUserID);
+      console.log('events that i am participant', data);
+    });
+  }, [userID]);
+
+  return (
+    <Container>
+      {ownerEvents && (
+        <>
+          <h2 style={{ textAlign: 'center' }}>Created Events</h2>
+          <Grid columns={3}>
+            {Object.entries(ownerEvents)?.map(
+              ([id, event]: [string, NewEvent], index) => (
+                <Grid.Row key={index}>
+                  <Grid.Column width={4}>
+                    <img
+                      className="ui tiny image"
+                      src="../../assets/69DFE2D3-0914-4DDB-94BC-E425304646E7.jpg"
+                    />
+                  </Grid.Column>
+                  <Grid.Column width={8}>
+                    <p>{event.name}</p>
+                    <p>{event.description}</p>
+                    <p>{event.url}</p>
+                    <p>{event.creationDate}</p>
+                    <label>
+                      מס משתתפים: {Object.keys(event.subscribers).length}
+                    </label>
+
+                    <UsersPhotos
+                      subscribers={event.subscribers}
+                      users={users}
+                    />
+                  </Grid.Column>
+                  <Grid.Column width={4}>
+                    <Link to={`/uploadFile/${id}`}>Upload images</Link>
+                    <Button>Details</Button>
+                  </Grid.Column>
+                </Grid.Row>
+              )
+            )}
+          </Grid>
+        </>
+      )}
+    </Container>
+  );
+};
+
+interface UsersPhotosProps {
+  subscribers: Record<string, boolean>;
+  users?: Record<string, UserState>;
+}
+const UsersPhotos: FC<UsersPhotosProps> = ({ subscribers, users }) => {
+  const ids = Object.keys(subscribers);
+  const handleImageError = (e: any) => {
+    e.target.src = userIMAGE;
+  };
+  return (
+    <>
+      {users &&
+        Object.entries(users)
+          ?.filter(([k, v]) => ids.includes(k))
+          .map(([k, v]) => (
+            <img
+              title={`${v.firstName} ${v.lastName}`}
+              style={{ width: '2em', borderRadius: '50%' }}
+              src={v.pictureUrl}
+              onError={handleImageError}
+              onClick={() => navigator.clipboard.writeText(k)}
+            />
+          ))}
+    </>
+  );
+};
