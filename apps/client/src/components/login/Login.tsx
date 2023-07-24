2427f66039faf4a93cdc582076fd8a760e50455e
YoelAharoni12 <yoel345@gmail.com>
2023-07-24 11:11:35 +0300

    change design login page and change icon in create event modal

--- apps/client/src/components/login/Login.tsx
+++ apps/client/src/components/login/Login.tsx
@@ -1,23 +1,22 @@
-import React, { useEffect } from 'react';
+import React, {useEffect} from 'react';
 import jwt_decode from 'jwt-decode';
-import { setUser, UserState } from '../../store/reducers/userSlice';
-import { useLocation, useNavigate } from 'react-router-dom';
-import { useDispatch } from 'react-redux';
-import { CredentialResponse, GoogleLogin } from '@react-oauth/google';
-import { toast } from 'react-toastify';
-import { useCookies } from 'react-cookie';
-import { orderByChild, push, equalTo, query, get } from 'firebase/database';
-import { usersRef } from '../../helpers/firebase';
+import {setUser, UserState} from '../../store/reducers/userSlice';
+import {useLocation, useNavigate} from 'react-router-dom';
+import {useDispatch} from 'react-redux';
+import {CredentialResponse, GoogleLogin} from '@react-oauth/google';
+import {toast} from 'react-toastify';
+import {useCookies} from 'react-cookie';
+import {equalTo, get, orderByChild, push, query} from 'firebase/database';
+import {usersRef} from '../../helpers/firebase';
 import FacebookLogin from '@greatsumini/react-facebook-login';
-import AppleLogin from 'react-apple-login';
-import { AppleFilled } from '@ant-design/icons';
-import { useNavigation } from '../../hooks/navigate';
-import { redirect } from 'react-router-dom';
+import {AppleFilled} from '@ant-design/icons';
+import {useNavigation} from '../../hooks/navigate';
 import './Login.less';
+import {Header} from 'semantic-ui-react';
 
 const Login: React.FC = () => {
   const dispatch = useDispatch();
-  const { goToMyEventsPage } = useNavigation();
+  const {goToMyEventsPage} = useNavigation();
   const navigate = useNavigate();
   const location = useLocation();
   const [cookies, setCookie] = useCookies(['user']);
@@ -43,7 +42,7 @@ const Login: React.FC = () => {
         sub: responsePayload.sub,
       };
 
-      let user: UserState = { ...tempUser };
+      let user: UserState = {...tempUser};
       const userWithSameSub = await get(
         query(usersRef, orderByChild('sub'), equalTo(responsePayload.sub))
       );
@@ -52,11 +51,11 @@ const Login: React.FC = () => {
         userWithSameSub.forEach((childSnapshot) => {
           const childKey = childSnapshot.key;
           const childData = childSnapshot.val();
-          user = { ...childData, id: childKey };
+          user = {...childData, id: childKey};
         });
       } else {
         const newEventRef = push(usersRef, tempUser);
-        user = { ...tempUser, id: newEventRef.key! };
+        user = {...tempUser, id: newEventRef.key!};
       }
 
       if (from) {
@@ -64,7 +63,7 @@ const Login: React.FC = () => {
       }
 
       toast.success(`welcome ${user.firstName} ${user.lastName}`);
-      setCookie('user', JSON.stringify(user), { path: '/' });
+      setCookie('user', JSON.stringify(user), {path: '/'});
       dispatch(setUser(user));
     }
   };
@@ -75,46 +74,51 @@ const Login: React.FC = () => {
   };
 
   return (
-    <div className="main-page">
-      <h1>lets get started </h1>
-      <div className="googke-signon">
-        <GoogleLogin
-          useOneTap
-          onSuccess={(response) =>
-            handleSuccess(response, location.state?.from)
-          }
-          onError={handleError}
-        />
-      </div>
+    <>
+      <div className="main-page">
+        <Header className={"login-header"} as='h1'>Login to your account</Header>
+        <div className={"buttons-login-section"} style={{margin:'7em'}}>
+          <div className="googke-signon">
+            <GoogleLogin
+              useOneTap
+              onSuccess={(response) =>
+                handleSuccess(response, location.state?.from)
+              }
+              onError={handleError}
+            />
+          </div>
 
-      <button className="apple-signin-button">
-        <div className="apple-logo-wrapper">
-          <AppleFilled
-            rev
-            src="apple-logo.png"
-            alt="Apple logo"
-            className="apple-logo"
-          />
+          <button className="apple-signin-button">
+            <div className="apple-logo-wrapper">
+              <AppleFilled
+                rev
+                src="apple-logo.png"
+                alt="Apple logo"
+                className="apple-logo"
+              />
+            </div>
+            <span className="apple-signin-text">Sign in with Apple</span>
+          </button>
+          <div className="facebook">
+            <FacebookLogin
+              appId={'appId'}
+              initParams={{
+                version: 'v10.0',
+              }}
+              style={{
+                width:"max-content",
+                fontSize: '1.1em',
+                backgroundColor: '#4267b2',
+                color: '#fff',
+                padding: '0.5em 2.1em',
+                border: 'none',
+                borderRadius: '4px',
+              }}
+            />
+          </div>
         </div>
-        <span className="apple-signin-text">Sign in with Apple</span>
-      </button>
-      <div className="facebook">
-        <FacebookLogin
-          appId={'appId'}
-          initParams={{
-            version: 'v10.0',
-          }}
-          style={{
-            fontSize: '1.1em',
-            backgroundColor: '#4267b2',
-            color: '#fff',
-            padding: '0.5em 2.1em',
-            border: 'none',
-            borderRadius: '4px',
-          }}
-        />
       </div>
-    </div>
+    </>
   );
 };
 
