307123d4a70e11789e1e2a178045cb6fdf56ca27
YoelAharoni12 <yoel345@gmail.com>
2023-07-26 18:17:45 +0300

    change design upload

--- apps/client/src/components/aboutUs/AboutUs.tsx
+++ apps/client/src/components/aboutUs/AboutUs.tsx
@@ -1,41 +1,57 @@
-import { FC } from 'react';
-import { Card, Row } from 'antd';
+import {FC} from 'react';
+import {Card, Row} from 'antd';
 import './AboutUs.less';
+import {Fade} from "react-awesome-reveal";
 
-export interface AboutUsProps {}
+export interface AboutUsProps {
+}
 
 export const AboutUs: FC<AboutUsProps> = ({}) => {
   return (
-    <Row className="images-con" gutter={16}>
-      <Card
-        hoverable
-        cover={<img src="../../assets/pic.jpeg" />}
-        style={{ margin: '1em' }}
-      >
-        <Card.Meta title="yoal ahroni" description="www.instagram.com" />
-      </Card>
-      <Card
-        hoverable
-        cover={<img src="../../assets/pic.jpeg" />}
-        style={{ margin: '1em' }}
-      >
-        <Card.Meta title="ohad ben shimol" description="www.instagram.com" />
-      </Card>
-      <Card
-        hoverable
-        cover={<img src="../../assets/pic.jpeg" />}
-        style={{ margin: '1em' }}
-      >
-        <Card.Meta title="oriel yemini" description="www.instagram.com" />
-      </Card>
-      <Card
-        hoverable
-        cover={<img src="../../assets/pic.jpeg" />}
-        style={{ margin: '1em' }}
-      >
-        <Card.Meta title="moshe aviten" description="www.instagram.com" />
-      </Card>
-    </Row>
+    <>
+      <Row className="images-con" gutter={16}>
+        <Card
+          hoverable
+          cover={<img src="../../assets/pic.jpeg"/>}
+          style={{margin: '1em'}}
+        >
+          <Card.Meta title="Yoel Ahroni" description="Chief Fun Officer"/>
+        </Card>
+        <Card
+          hoverable
+          cover={<img src="../../assets/pic.jpeg"/>}
+          style={{margin: '1em'}}
+        >
+          <Card.Meta title="Ohad Ben Shimol" description="Digital Ninja"/>
+        </Card>
+        <Card
+          hoverable
+          cover={<img src="../../assets/pic.jpeg"/>}
+          style={{margin: '1em'}}
+        >
+          <Card.Meta title="Oriel Yemini" description="Algorithm DJ"/>
+        </Card>
+        <Card
+          hoverable
+          cover={<img src="../../assets/pic.jpeg"/>}
+          style={{margin: '1em'}}
+        >
+          <Card.Meta title="Moshe Avitan" description="404 Error Therapist"/>
+        </Card>
+      </Row>
+      <div>
+        <Fade
+          className="fade-con"
+          direction="right"
+          duration={50}
+          cascade
+          style={{fontSize: '1.5em'}}
+        >
+          Our company specializes in cutting-edge facial recognition technology, offering an image-upload
+          application that accurately identifies and verifies faces with exceptional precision.
+        </Fade>
+      </div>
+    </>
   );
 };
 
