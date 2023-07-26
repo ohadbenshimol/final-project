3a774bc9e9554e725334eecc5e8dc5ec2567d94a
ohad ben shimol <a@a.com>
2023-07-26 13:53:18 +0300

    wip

--- apps/client/src/components/mainPage/MainPage.tsx
+++ apps/client/src/components/mainPage/MainPage.tsx
@@ -4,6 +4,26 @@ import { Button } from 'antd';
 import { useNavigation } from '../../hooks/navigate';
 import './MainPage.less';
 
+import PropTypes from 'prop-types';
+interface BezelsIphoneProps {
+  color: string;
+  className: string; // The 'className' prop is optional and has a default type of 'string'
+}
+export const BezelsIphone: React.FC<BezelsIphoneProps> = ({
+  color,
+  className,
+}) => {
+  return (
+    <div className={`bezels-iphone ${className}`}>
+      <img
+        className="iphone-pro-deep"
+        alt="Iphone pro deep"
+        src={'../../assets/a.png'}
+      />
+    </div>
+  );
+};
+
 const MainPage: React.FC = () => {
   const user = userIsLoggedIn();
   const { goToLoginPage, goToMyEventsPage: goToToMyEventsPage } =
@@ -11,17 +31,16 @@ const MainPage: React.FC = () => {
 
   return (
     <div>
+      <BezelsIphone className="color-gray" color="gray" />;
       <h1>מלא טקסט עם אנימציות ואיזה תמונת רקע </h1>
       <h1>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ullam</h1>
       <h1>
         doloremque sequi explicabo, laborum cumque atque perspiciatis impedit
       </h1>
-
       <h1>
         voluptates quisquam debitis blanditiis repellat. Est consequatur ea
         molestias nemo, saepe vel ipsam?
       </h1>
-
       {!user ? (
         <Button onClick={goToLoginPage}>login</Button>
       ) : (
