ab890527b1fa6071c33d982bb1eb2d5e82626915
ohad ben shimol <ohadbenshimol3@gmail.com>
2023-07-30 09:02:34 +0300

    .

--- apps/client/src/components/mainPage/MainPage.tsx
+++ apps/client/src/components/mainPage/MainPage.tsx
@@ -1,17 +1,102 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { userIsLoggedIn } from '../../store/reducers/userSlice';
 import { useNavigation } from '../../hooks/navigate';
+import { Button } from 'antd';
+import { Fade } from 'react-awesome-reveal';
+import Particles from 'react-particles';
+import { loadSlim } from 'tsparticles-slim';
 import './MainPage.less';
-import { Button, Image } from 'antd';
-import { AttentionSeeker, Fade, Reveal, Slide } from 'react-awesome-reveal';
 
 const MainPage: React.FC = () => {
   const user = userIsLoggedIn();
   const { goToLoginPage, goToMyEventsPage: goToToMyEventsPage } =
     useNavigation();
 
+  const particlesInit = useCallback(async (engine: any) => {
+    console.log(engine);
+    await loadSlim(engine);
+  }, []);
+
+  const particlesLoaded = useCallback(async (container: any) => {
+    console.log(container);
+  }, []);
+
   return (
-    <div className="main-con">
+    <div className="main-page-con">
+      <Particles
+        id="tsparticles"
+        init={particlesInit}
+        loaded={particlesLoaded}
+        options={{
+          background: {
+            color: {
+              value: 'transparent',
+            },
+          },
+          fpsLimit: 120,
+          interactivity: {
+            events: {
+              onClick: {
+                enable: true,
+                mode: 'push',
+              },
+              onHover: {
+                enable: true,
+                mode: 'repulse',
+              },
+              resize: true,
+            },
+            modes: {
+              push: {
+                quantity: 4,
+              },
+              repulse: {
+                distance: 200,
+                duration: 0.4,
+              },
+            },
+          },
+          particles: {
+            color: {
+              value: 'black',
+            },
+            links: {
+              color: '#408378',
+              distance: 300,
+              enable: true,
+              opacity: 1,
+              width: 2,
+            },
+            move: {
+              direction: 'none',
+              enable: true,
+              outModes: {
+                default: 'bounce',
+              },
+              random: false,
+              speed: 2,
+              straight: false,
+            },
+            number: {
+              density: {
+                enable: true,
+                area: 1000,
+              },
+              value: 80,
+            },
+            opacity: {
+              value: 0.5,
+            },
+            shape: {
+              type: 'circle',
+            },
+            size: {
+              value: { min: 1, max: 5 },
+            },
+          },
+          detectRetina: true,
+        }}
+      />
       <div className="text-and-btn">
         <div className="first-text" style={{ display: 'flex' }}>
           <Fade cascade direction="left">
